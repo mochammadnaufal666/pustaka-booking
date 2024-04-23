ad81f2eb70d5e61a22729eccae45eda43f519ec9
WP2_Moch naufal <muhammadnau12@gmail.com>
2024-04-23 16:28:18 +0700

    enter

---
+++ application/controllers/Daftar.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Daftar extends CI_Controller {
+    public function index() {
+        // Memuat library form validation
+        $this->load->library('form_validation');
+        
+        $this->load->view('view-form-daftar');
+    }
+
+    public function proses_daftar() {
+        // Memuat library form validation
+        $this->load->library('form_validation');
+
+        // Mengatur aturan validasi
+        $this->form_validation->set_rules('nim', 'NIM', 'required');
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('tanggal_lahir', 'Tanggal Lahir', 'required');
+        $this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('view-form-daftar');
+        } else {
+            $data = array(
+                'nim' => $this->input->post('nim'),
+                'nama' => $this->input->post('nama'),
+                'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+                'tempat_lahir' => $this->input->post('tempat_lahir'),
+                'alamat' => $this->input->post('alamat')
+            );
+            $this->load->view('view-data-daftar', $data);
+        }
+    }
+}
+?>
