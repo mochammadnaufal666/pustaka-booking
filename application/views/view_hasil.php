8f5bb91e06cfa9e106793341436378a4b86d95d0
WP2_Moch naufal <muhammadnau12@gmail.com>
2024-05-07 16:43:21 +0700

    enter

---
+++ application/views/view_hasil.php
@@ -0,0 +1,7 @@
+
+<h2>Hasil Transaksi</h2>
+
+<p>Nama Pembeli: <?php echo $nama_pembeli;?></p>
+<p>No HP: <?php echo $no_hp;?></p>
+<p>Merk Sepatu: <?php echo $merk_sepatu;?> (Rp <?php echo number_format($harga_sepatu, 0, ',', '.');?>)</p>
+<p>Ukuran Sepatu: <?php echo $ukuran_sepatu;?></p>
